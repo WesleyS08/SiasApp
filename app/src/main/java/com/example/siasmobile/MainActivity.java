90403420cdfc12a68806957cf141b1fb400b51cf
Wesley <113936684+WesleyS08@users.noreply.github.com>
2024-08-23 09:09:36 -0300

    :bug:  Fix correção de inicialização
    - por algum motivo o codigo subiu vazio

--- app/src/main/java/com/example/siasmobile/MainActivity.java
+++ app/src/main/java/com/example/siasmobile/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.siasmobile;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.siasmobile.Onboarding.OnboardingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Verifica no SharedPreferences se o onboarding já foi completado
+        SharedPreferences sharedPref = getSharedPreferences("onboarding", MODE_PRIVATE);
+        boolean onboardingCompleted = sharedPref.getBoolean("completed", false);
+
+        if (!onboardingCompleted) {
+            // Se o onboarding não foi completado, redireciona para a OnboardingActivity
+            Intent intent = new Intent(this, OnboardingActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 }
--- app/src/main/java/com/example/siasmobile/Onboarding/OnboardingActivity.java
+++ app/src/main/java/com/example/siasmobile/Onboarding/OnboardingActivity.java
@@ -4,6 +4,7 @@ import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
@@ -25,21 +26,22 @@ public class OnboardingActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private OnboardingPagerAdapter onboardingPagerAdapter;
     private Button btnFinishOnboarding;
-    private TextView step1, step2, step3;
+    private TextView[] steps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
-
         viewPager = findViewById(R.id.viewPager);
         btnFinishOnboarding = findViewById(R.id.btnFinishOnboarding);
 
-        // Encontre os TextViews das etapas
-        step1 = findViewById(R.id.step1);
-        step2 = findViewById(R.id.step2);
-        step3 = findViewById(R.id.step3);
+        // Inicializando TextViews das etapas
+        steps = new TextView[]{
+                findViewById(R.id.step1),
+                findViewById(R.id.step2),
+                findViewById(R.id.step3)
+        };
 
         // Lista de layouts das telas
         List<Integer> layouts = new ArrayList<>();
@@ -54,68 +56,44 @@ public class OnboardingActivity extends AppCompatActivity {
         hideSystemUI();
 
         // Inicialmente, defina o fundo da primeira etapa como laranja
-        setStepBackground(step1, R.color.laranjaPrincipal);
-        step1.setTextColor(getResources().getColor(R.color.white));
+        setStepBackground(steps[0], R.color.laranjaPrincipal);
+        steps[0].setTextColor(getResources().getColor(R.color.white));
 
-        // Listener para mudar a visibilidade do botão e animar o progresso das etapas
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
-                // Exibe o botão "Começar" apenas na última tela
-                if (position == layouts.size() - 1) {
-                    btnFinishOnboarding.setVisibility(View.VISIBLE);
-                } else {
-                    btnFinishOnboarding.setVisibility(View.GONE);
-                }
+                btnFinishOnboarding.setVisibility(position == layouts.size() - 1 ? View.VISIBLE : View.GONE);
 
                 // Reseta o estado das etapas
                 resetStepStates();
 
                 // Atualiza a visibilidade dos ícones de checagem e anima o progresso
-                switch (position) {
-                    case 0:
-                        animateStepTransition(step1);
-                        break;
-                    case 1:
-                        animateStepTransition(step2);
-                        break;
-                    case 2:
-                        animateStepTransition(step3);
-                        break;
+                if (position < steps.length) {
+                    animateStepTransition(steps[position]);
                 }
             }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {}
         });
 
-        btnFinishOnboarding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Salva no SharedPreferences que o onboarding foi concluído
-                SharedPreferences sharedPref = getSharedPreferences("onboarding", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putBoolean("completed", true);
-                editor.apply();
+        btnFinishOnboarding.setOnClickListener(v -> {
+            SharedPreferences sharedPref = getSharedPreferences("onboarding", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean("completed", true);
+            editor.apply();
 
-                // Navega para a MainActivity
-                Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+            Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
+    //======================================================= Parte que oculta os btn e a navbar do celular =================================================
     private void hideSystemUI() {
-        // Hides the system UI
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
     }
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -123,41 +101,39 @@ public class OnboardingActivity extends AppCompatActivity {
             hideSystemUI();
         }
     }
+    //===========================================================================================================================================
+
+    //===================================================================Reserta as cores do progress bar========================================================================
 
     private void resetStepStates() {
-        // Reseta o fundo de todas as etapas para branco
-        resetStepBackgrounds();
-
-        // Define a cor do texto para todas as etapas
-        step1.setTextColor(getResources().getColor(R.color.laranjaPrincipal));
-        step2.setTextColor(getResources().getColor(R.color.laranjaPrincipal));
-        step3.setTextColor(getResources().getColor(R.color.laranjaPrincipal));
-    }
-
-    private void resetStepBackgrounds() {
-        // Define o fundo das etapas como branco
-        setStepBackground(step1, R.color.white);
-        setStepBackground(step2, R.color.white);
-        setStepBackground(step3, R.color.white);
+        for (TextView step : steps) {
+            setStepBackground(step, R.color.white);
+            step.setTextColor(getResources().getColor(R.color.laranjaPrincipal));
+        }
     }
 
     private void setStepBackground(TextView step, int color) {
-        // Atualiza a cor de fundo do TextView
-        GradientDrawable backgroundDrawable = (GradientDrawable) step.getBackground();
-        backgroundDrawable.setColor(getResources().getColor(color));
+        Drawable background = step.getBackground();
+        if (background instanceof GradientDrawable) {
+            GradientDrawable backgroundDrawable = (GradientDrawable) background;
+            backgroundDrawable.setColor(getResources().getColor(color));
+        } else {
+            step.setBackgroundColor(getResources().getColor(color));
+        }
     }
+    //===========================================================================================================================================
 
+
+    //==============================================================================Animacao  para ficar bonitinho=============================================================
     private void animateStepTransition(TextView activeStep) {
-        // Animação da cor de fundo da etapa ativa
+
         GradientDrawable backgroundDrawable = (GradientDrawable) activeStep.getBackground();
         int colorFrom = getResources().getColor(R.color.white);
         int colorTo = getResources().getColor(R.color.laranjaPrincipal);
 
         ValueAnimator backgroundColorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
-        backgroundColorAnimator.setDuration(300); // Duração da animação
-        backgroundColorAnimator.addUpdateListener(animator -> {
-            backgroundDrawable.setColor((int) animator.getAnimatedValue());
-        });
+        backgroundColorAnimator.setDuration(300);
+        backgroundColorAnimator.addUpdateListener(animator -> backgroundDrawable.setColor((int) animator.getAnimatedValue()));
         backgroundColorAnimator.start();
 
         // Animação da cor do texto da etapa ativa
@@ -165,10 +141,8 @@ public class OnboardingActivity extends AppCompatActivity {
         int textColorTo = getResources().getColor(android.R.color.white);
 
         ValueAnimator textColorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), textColorFrom, textColorTo);
-        textColorAnimator.setDuration(500); // Duração da animação
-        textColorAnimator.addUpdateListener(animator -> {
-            activeStep.setTextColor((int) animator.getAnimatedValue());
-        });
+        textColorAnimator.setDuration(500);
+        textColorAnimator.addUpdateListener(animator -> activeStep.setTextColor((int) animator.getAnimatedValue()));
         textColorAnimator.start();
     }
 }
